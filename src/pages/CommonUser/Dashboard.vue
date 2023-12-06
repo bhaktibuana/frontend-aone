a918aef4a64c8cf92e667dd4ab9f22cf212926c6
bhaktibuana <bhaktibuana19@gmail.com>
2023-12-06 23:41:54 +0700

    feat(auth): Integration Logout (dummy slicing)

--- src/pages/CommonUser/Dashboard.vue
+++ src/pages/CommonUser/Dashboard.vue
@@ -1,5 +1,37 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useRouter } from "vue-router";
+import { notification } from "ant-design-vue";
 
-<template><h2>Common User Dashboard</h2></template>
+import { APILogout } from "@/apis/Authentication/Logout";
+
+import BaseButton from "@/components/base/Button/Button.vue";
+
+import { removeCookie } from "@/utils/functions/cookie";
+
+const router = useRouter();
+
+const handleLogout = async (): Promise<void> => {
+  try {
+    await APILogout.logout();
+    removeCookie("accessToken");
+    router.push({ name: "Login" });
+    history.pushState(null, "", "");
+  } catch (error) {
+    notification["error"]({
+      message: "Error",
+      description: "Logout Failed",
+      placement: "bottom",
+    });
+  }
+};
+</script>
+
+<template>
+  <h2>Common User Dashboard</h2>
+
+  <div @click="handleLogout">
+    <base-button label="Logout" />
+  </div>
+</template>
 
 <style scoped lang="scss"></style>
